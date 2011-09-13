91556c23fd92688f4023aaa534c735c5b2d3e111
SND\Etheral_cp <SND\Etheral_cp@937343c3-697a-4e39-b5c6-83b50a1b7d63>
2011-09-13 05:48:15 +0000

    Started Work on Wireless Synx
    git-svn-id: https://opensynx.svn.codeplex.com/svn@1017 937343c3-697a-4e39-b5c6-83b50a1b7d63

---
+++ Synx/AirSynx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppLimit.CloudComputing.SharpBox;
+using AppLimit.CloudComputing.SharpBox.StorageProvider.DropBox;
+
+namespace AirSynx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorage dropBoxStorage = new CloudStorage();
+            var dropBoxConfig = CloudStorage.GetCloudConfigurationEasy(nSupportedCloudConfigurations.DropBox);
+            var dropBoxCredentials = new DropBoxCredentials();
+            dropBoxCredentials.ConsumerKey = "zip1sn9ee6cbnh3";
+            dropBoxCredentials.ConsumerSecret = "8l5kntfytecpuga";
+            dropBoxCredentials.UserName = "admin@etheralstudios.com";
+            dropBoxCredentials.Password = "superxl1";
+            var storageToken = dropBoxStorage.Open(dropBoxConfig, dropBoxCredentials);
+            dropBoxStorage.CreateFolder("/Synx");
+            var Folder = dropBoxStorage.GetFolder("/Synx");
+            String srcFile = Environment.ExpandEnvironmentVariables("C:\\Users\\Nicholas\\Test.txt");
+            dropBoxStorage.UploadFile(srcFile, Folder);
+            dropBoxStorage.Close();
+        }
+    }
+}
