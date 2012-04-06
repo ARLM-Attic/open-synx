d185df237fd6948b21914bdb23135833b615a5ac
SND\Etheral_cp <SND\Etheral_cp@937343c3-697a-4e39-b5c6-83b50a1b7d63>
2012-04-06 03:07:49 +0000

    Bug fixes and some more features for Synx 2.1
    git-svn-id: https://opensynx.svn.codeplex.com/svn@14335 937343c3-697a-4e39-b5c6-83b50a1b7d63

--- SynxProject/Synx/Form1.Designer.vb
+++ SynxProject/Synx/Form1.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Home
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Home))
         Me.SynxButton = New System.Windows.Forms.Button()
         Me.DevDrv = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.AboutButton = New System.Windows.Forms.Button()
         Me.IdeaButton = New System.Windows.Forms.Button()
         Me.PatchButton = New System.Windows.Forms.Button()
@@ -45,14 +44,16 @@ Partial Class Home
         Me.iTunesContentButton = New System.Windows.Forms.Button()
         Me.WindowsContentButton = New System.Windows.Forms.Button()
         Me.DCIMCheckBox = New System.Windows.Forms.CheckBox()
-        Me.exeptionLabel = New System.Windows.Forms.Label()
-        Me.AirSynxButton = New System.Windows.Forms.Button()
         Me.HelpSiteButton = New System.Windows.Forms.Button()
         Me.MusicBrowse = New System.Windows.Forms.Button()
         Me.VideosBrowse = New System.Windows.Forms.Button()
         Me.MoviesBrowse = New System.Windows.Forms.Button()
         Me.PicturesBrowse = New System.Windows.Forms.Button()
         Me.DevDrvBrowse = New System.Windows.Forms.Button()
+        Me.DevDrvRButton = New System.Windows.Forms.RadioButton()
+        Me.ASXRButton = New System.Windows.Forms.RadioButton()
+        Me.ASXUsername = New System.Windows.Forms.TextBox()
+        Me.ASXPassword = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'SynxButton
@@ -60,33 +61,23 @@ Partial Class Home
         Me.SynxButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 38.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.SynxButton.Location = New System.Drawing.Point(437, 293)
         Me.SynxButton.Name = "SynxButton"
-        Me.SynxButton.Size = New System.Drawing.Size(188, 178)
+        Me.SynxButton.Size = New System.Drawing.Size(198, 143)
         Me.SynxButton.TabIndex = 0
         Me.SynxButton.Text = "SYNX!"
         Me.SynxButton.UseVisualStyleBackColor = True
         '
         'DevDrv
         '
-        Me.DevDrv.Location = New System.Drawing.Point(445, 50)
+        Me.DevDrv.Location = New System.Drawing.Point(437, 52)
         Me.DevDrv.MaxLength = 13
         Me.DevDrv.Name = "DevDrv"
         Me.DevDrv.Size = New System.Drawing.Size(82, 20)
         Me.DevDrv.TabIndex = 8
         Me.DevDrv.Text = "E:\"
         '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(442, 29)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(145, 18)
-        Me.Label4.TabIndex = 7
-        Me.Label4.Text = "Android Device Drive"
-        '
         'AboutButton
         '
-        Me.AboutButton.Location = New System.Drawing.Point(321, 310)
+        Me.AboutButton.Location = New System.Drawing.Point(321, 293)
         Me.AboutButton.Name = "AboutButton"
         Me.AboutButton.Size = New System.Drawing.Size(75, 23)
         Me.AboutButton.TabIndex = 9
@@ -95,7 +86,7 @@ Partial Class Home
         '
         'IdeaButton
         '
-        Me.IdeaButton.Location = New System.Drawing.Point(6, 310)
+        Me.IdeaButton.Location = New System.Drawing.Point(6, 293)
         Me.IdeaButton.Name = "IdeaButton"
         Me.IdeaButton.Size = New System.Drawing.Size(113, 23)
         Me.IdeaButton.TabIndex = 16
@@ -104,7 +95,7 @@ Partial Class Home
         '
         'PatchButton
         '
-        Me.PatchButton.Location = New System.Drawing.Point(125, 310)
+        Me.PatchButton.Location = New System.Drawing.Point(125, 293)
         Me.PatchButton.Name = "PatchButton"
         Me.PatchButton.Size = New System.Drawing.Size(190, 23)
         Me.PatchButton.TabIndex = 17
@@ -114,7 +105,7 @@ Partial Class Home
         'FollowButton
         '
         Me.FollowButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.FollowButton.Location = New System.Drawing.Point(6, 339)
+        Me.FollowButton.Location = New System.Drawing.Point(6, 322)
         Me.FollowButton.Name = "FollowButton"
         Me.FollowButton.Size = New System.Drawing.Size(113, 95)
         Me.FollowButton.TabIndex = 19
@@ -235,7 +226,7 @@ Partial Class Home
         '
         'iTunesContentButton
         '
-        Me.iTunesContentButton.Location = New System.Drawing.Point(445, 105)
+        Me.iTunesContentButton.Location = New System.Drawing.Point(437, 252)
         Me.iTunesContentButton.Name = "iTunesContentButton"
         Me.iTunesContentButton.Size = New System.Drawing.Size(109, 23)
         Me.iTunesContentButton.TabIndex = 33
@@ -244,7 +235,7 @@ Partial Class Home
         '
         'WindowsContentButton
         '
-        Me.WindowsContentButton.Location = New System.Drawing.Point(445, 76)
+        Me.WindowsContentButton.Location = New System.Drawing.Point(437, 223)
         Me.WindowsContentButton.Name = "WindowsContentButton"
         Me.WindowsContentButton.Size = New System.Drawing.Size(160, 23)
         Me.WindowsContentButton.TabIndex = 34
@@ -261,28 +252,9 @@ Partial Class Home
         Me.DCIMCheckBox.Text = "Back up Recorded Pictures and Videos"
         Me.DCIMCheckBox.UseVisualStyleBackColor = True
         '
-        'exeptionLabel
-        '
-        Me.exeptionLabel.AutoSize = True
-        Me.exeptionLabel.Location = New System.Drawing.Point(3, 461)
-        Me.exeptionLabel.Name = "exeptionLabel"
-        Me.exeptionLabel.Size = New System.Drawing.Size(355, 13)
-        Me.exeptionLabel.TabIndex = 36
-        Me.exeptionLabel.Text = "When you Synx you may get an exeption, just click continue and ignore it."
-        '
-        'AirSynxButton
-        '
-        Me.AirSynxButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.AirSynxButton.Location = New System.Drawing.Point(125, 339)
-        Me.AirSynxButton.Name = "AirSynxButton"
-        Me.AirSynxButton.Size = New System.Drawing.Size(190, 97)
-        Me.AirSynxButton.TabIndex = 37
-        Me.AirSynxButton.Text = "AirSynx"
-        Me.AirSynxButton.UseVisualStyleBackColor = True
-        '
         'HelpSiteButton
         '
-        Me.HelpSiteButton.Location = New System.Drawing.Point(321, 339)
+        Me.HelpSiteButton.Location = New System.Drawing.Point(125, 322)
         Me.HelpSiteButton.Name = "HelpSiteButton"
         Me.HelpSiteButton.Size = New System.Drawing.Size(75, 23)
         Me.HelpSiteButton.TabIndex = 38
@@ -332,27 +304,70 @@ Partial Class Home
         'DevDrvBrowse
         '
         Me.DevDrvBrowse.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
-        Me.DevDrvBrowse.Location = New System.Drawing.Point(532, 48)
+        Me.DevDrvBrowse.Location = New System.Drawing.Point(525, 50)
         Me.DevDrvBrowse.Name = "DevDrvBrowse"
         Me.DevDrvBrowse.Size = New System.Drawing.Size(75, 23)
         Me.DevDrvBrowse.TabIndex = 44
         Me.DevDrvBrowse.Text = "Browse..."
         Me.DevDrvBrowse.UseVisualStyleBackColor = True
         '
+        'DevDrvRButton
+        '
+        Me.DevDrvRButton.AutoSize = True
+        Me.DevDrvRButton.Checked = True
+        Me.DevDrvRButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DevDrvRButton.Location = New System.Drawing.Point(437, 24)
+        Me.DevDrvRButton.Name = "DevDrvRButton"
+        Me.DevDrvRButton.Size = New System.Drawing.Size(179, 22)
+        Me.DevDrvRButton.TabIndex = 45
+        Me.DevDrvRButton.TabStop = True
+        Me.DevDrvRButton.Text = "Synx to Android device:"
+        Me.DevDrvRButton.UseVisualStyleBackColor = True
+        '
+        'ASXRButton
+        '
+        Me.ASXRButton.AutoSize = True
+        Me.ASXRButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ASXRButton.Location = New System.Drawing.Point(437, 80)
+        Me.ASXRButton.Name = "ASXRButton"
+        Me.ASXRButton.Size = New System.Drawing.Size(132, 22)
+        Me.ASXRButton.TabIndex = 46
+        Me.ASXRButton.Text = "Synx to AirSynx:"
+        Me.ASXRButton.UseVisualStyleBackColor = True
+        '
+        'ASXUsername
+        '
+        Me.ASXUsername.Location = New System.Drawing.Point(437, 108)
+        Me.ASXUsername.Name = "ASXUsername"
+        Me.ASXUsername.Size = New System.Drawing.Size(179, 20)
+        Me.ASXUsername.TabIndex = 47
+        Me.ASXUsername.Text = "AirSynx Username"
+        '
+        'ASXPassword
+        '
+        Me.ASXPassword.Location = New System.Drawing.Point(437, 134)
+        Me.ASXPassword.Name = "ASXPassword"
+        Me.ASXPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.ASXPassword.Size = New System.Drawing.Size(179, 20)
+        Me.ASXPassword.TabIndex = 48
+        Me.ASXPassword.Text = "AirSynx Password"
+        '
         'Home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(631, 477)
+        Me.ClientSize = New System.Drawing.Size(641, 442)
+        Me.Controls.Add(Me.ASXPassword)
+        Me.Controls.Add(Me.ASXUsername)
+        Me.Controls.Add(Me.ASXRButton)
+        Me.Controls.Add(Me.DevDrvRButton)
         Me.Controls.Add(Me.DevDrvBrowse)
         Me.Controls.Add(Me.PicturesBrowse)
         Me.Controls.Add(Me.MoviesBrowse)
         Me.Controls.Add(Me.VideosBrowse)
         Me.Controls.Add(Me.MusicBrowse)
         Me.Controls.Add(Me.HelpSiteButton)
-        Me.Controls.Add(Me.AirSynxButton)
-        Me.Controls.Add(Me.exeptionLabel)
         Me.Controls.Add(Me.DCIMCheckBox)
         Me.Controls.Add(Me.WindowsContentButton)
         Me.Controls.Add(Me.iTunesContentButton)
@@ -373,7 +388,6 @@ Partial Class Home
         Me.Controls.Add(Me.IdeaButton)
         Me.Controls.Add(Me.AboutButton)
         Me.Controls.Add(Me.DevDrv)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.SynxButton)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -387,7 +401,6 @@ Partial Class Home
     End Sub
     Friend WithEvents SynxButton As System.Windows.Forms.Button
     Friend WithEvents DevDrv As System.Windows.Forms.TextBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents AboutButton As System.Windows.Forms.Button
     Friend WithEvents IdeaButton As System.Windows.Forms.Button
     Friend WithEvents PatchButton As System.Windows.Forms.Button
@@ -407,13 +420,15 @@ Partial Class Home
     Friend WithEvents iTunesContentButton As System.Windows.Forms.Button
     Friend WithEvents WindowsContentButton As System.Windows.Forms.Button
     Friend WithEvents DCIMCheckBox As System.Windows.Forms.CheckBox
-    Friend WithEvents exeptionLabel As System.Windows.Forms.Label
-    Friend WithEvents AirSynxButton As System.Windows.Forms.Button
     Friend WithEvents HelpSiteButton As System.Windows.Forms.Button
     Friend WithEvents MusicBrowse As System.Windows.Forms.Button
     Friend WithEvents VideosBrowse As System.Windows.Forms.Button
     Friend WithEvents MoviesBrowse As System.Windows.Forms.Button
     Friend WithEvents PicturesBrowse As System.Windows.Forms.Button
     Friend WithEvents DevDrvBrowse As System.Windows.Forms.Button
+    Friend WithEvents DevDrvRButton As System.Windows.Forms.RadioButton
+    Friend WithEvents ASXRButton As System.Windows.Forms.RadioButton
+    Friend WithEvents ASXUsername As System.Windows.Forms.TextBox
+    Friend WithEvents ASXPassword As System.Windows.Forms.TextBox
 
 End Class
