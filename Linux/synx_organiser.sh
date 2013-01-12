5654a019e9467ea5678ce4088406f612c8f1e97d
SND\Etheral_cp <SND\Etheral_cp@937343c3-697a-4e39-b5c6-83b50a1b7d63>
2013-01-12 05:39:52 +0000

    Modified the Synx Organiser
    git-svn-id: https://opensynx.svn.codeplex.com/svn@23334 937343c3-697a-4e39-b5c6-83b50a1b7d63

--- Linux/synx_organiser.sh
+++ Linux/synx_organiser.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 #Synx (c) Copyright Etheral Studios 2012
-#Synx Organiser v0.2
+#Synx Organiser v0.2.1
 
-echo "Synx (c) Copyright Etheral Studios 2012"
-echo "Synx for Linux v0.1.2 Alpha - for more information go to http://opensynx.codeplex.com"
+clear
+echo "Synx Organiser (c) Copyright Etheral Studios 2012"
+echo "Synx Organiser v0.2.1 Alpha - for more information go to http://opensynx.codeplex.com"
+echo ""
 
 cd /mnt/sdcard/
 
 if [ ! -d "Music" ] ; then
+    echo "No music direcotry found on device, making now..."
 	mkdir Music
 fi
 
 if [ ! -d "Pictures" ] ; then
+    echo "No pictures direcotry found on device, making now..."
 	mkdir Pictures
 fi
 
 if [ ! -d "DCIM" ] ; then
+    echo "No DCIM direcotry found on device, making now..."
 	mkdir DCIM
 fi
 
 if [ ! -d "Videos" ] ; then
+    echo "No videos direcotry found on device, making now..."
 	mkdir Videos
 fi
 
 if [ ! -d "Movies" ] ; then
+    echo "No movies direcotry found on device, making now..."
 	mkdir Movies
 fi
 
 if [ ! -d "Podcasts" ] ; then
+    echo "No podcasts direcotry found on device, making now..."
 	mkdir Podcasts
 fi
+echo ""
+echo "Back to you synx.sh"
